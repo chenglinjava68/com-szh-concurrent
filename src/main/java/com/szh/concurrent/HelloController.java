e4ab195e35058d36a8b33c4a949efd77d1fcd4d9
zhihao.song <1551151530@qq.com>
2016-12-27 08:45:11 +0800

    first push

---
+++ src/main/java/com/szh/concurrent/HelloController.java
@@ -0,0 +1,21 @@
+package com.szh.concurrent;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+	@RequestMapping(method = RequestMethod.GET)
+	public String printWelcome(ModelMap model) {
+		model.addAttribute("message", "Hello world!");
+
+		return "hello";
+	}
+
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+	}
+}
---
+++ src/main/java/com/szh/concurrent/User.java
@@ -0,0 +1,59 @@
+package com.szh.concurrent;
+public class User implements Cloneable{
+	private String name;
+	private int age;
+	
+	public User(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	public boolean equals(Object obj) {
+		if(this == obj) {
+			return true;
+		}
+		if(!(obj instanceof User)) {
+			return false;	
+		}
+		User user = (User)obj;
+		//if(this.name==user.name && this.age==user.age)
+		if(this.name.equals(user.name) 
+			&& this.age==user.age) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	public int hashCode() {
+		return name.hashCode() + age;
+	}
+	
+	public String toString() {
+		return "{name:'" + name + "',age:" + age + "}";
+	}
+	public Object clone()  {
+		Object object = null;
+		try {
+			object = super.clone();
+		} catch (CloneNotSupportedException e) {}
+		return object;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	static int i;	static String i2;
+
+	public static void main(String[] args) {
+		//String str;
+		// String [] str={"www","sss"};
+		String a="1"+"";
+		String b="1"+"";
+
+		System.out.println(a.);		System.out.println(b.hashCode());
+
+		//System.out.println(i);
+	}
+} 
